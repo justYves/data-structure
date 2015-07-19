e1373a47d43d8d72a892288cc8b78ea993dd9de0
Yves Yuen <yves.j.yuen@gmail.com>
2015-07-19 15:34:00 -0400

    added Set and Exercices

--- set/set.js
+++ set/set.js
@@ -1,9 +1,9 @@
-function Set(){
-this.data = [];
+function Set() {
+  this.data = [];
 }
 
 Set.prototype.add = function(value) {
-  if(this.data.indexOf(value) === -1){
+  if (this.data.indexOf(value) === -1) {
     this.data.push(value);
     return true;
   }
@@ -12,8 +12,8 @@ Set.prototype.add = function(value) {
 
 Set.prototype.remove = function(value) {
   var index = this.data.indexOf(value);
-  if(index > -1){
-    this.data.splice(index,1);
+  if (index > -1) {
+    this.data.splice(index, 1);
     return true;
   }
   return false;
@@ -25,14 +25,52 @@ Set.prototype.show = function() {
 
 //Helper function
 Set.prototype.contains = function(value) {
-  if(this.data.indexOf(value)){
+  if (this.data.indexOf(value) > -1) {
     return true;
   }
   return false;
 };
 
-Set.prototype.union = function(value) {
-  // body...
+Set.prototype.union = function(set) {
+  var tempSet = new Set();
+  for (var i = 0; i < this.data.length; i++) {
+    tempSet.add(this.data[i]);
+  }
+  for (var i = 0; i < set.data.length; i++) {
+    tempSet.add(set.data[i]);
+  }
+  return tempSet;
 };
+
+Set.prototype.intersect = function(set) {
+  var tempSet = new Set();
+  for (var i = 0; i < this.data.length; i++) {
+    if (set.contains(this.data[i])) tempSet.add(this.data[i]);
+  }
+  return tempSet;
+};
+
+Set.prototype.size = function() {
+  return this.data.length;
+};
+
+Set.prototype.subset = function(set) {
+  if (this.size() > set.size()) return false;
+  for (var i = 0; i < this.data.length; i++) {
+    if (!set.contains(this.data[i])) return false;
+  }
+  return true;
+};
+
+Set.prototype.difference = function(set) {
+  var tempSet = new Set();
+  for (var i = 0; i < this.data.length; i++) {
+    if (set.contains(this.data[i])) {
+      tempSet.add(this.data[i]);
+    }
+    return tempSet;
+  }
+};
+
 
 module.exports = Set;
--- set/setImpl.js
+++ set/setImpl.js
@@ -26,3 +26,134 @@ if (names.remove("Mike")) {
 } else {
   console.log(removed + " not removed.");
 }
+
+console.log("<--- UNION --->");
+
+
+var cis = new Set();
+cis.add("Mike");
+cis.add("Clayton");
+cis.add("Jennifer");
+cis.add("Raymond");
+var dmp = new Set();
+dmp.add("Raymond");
+dmp.add("Cynthia");
+dmp.add("Jonathan");
+dmp.add("Julie");
+var it = new Set();
+it = cis.union(dmp);
+console.log(it.show());
+
+
+console.log("<--- INTERSECTION --->");
+
+var cis = new Set();
+cis.add("Mike");
+cis.add("Clayton");
+cis.add("Jennifer");
+cis.add("Raymond");
+var dmp = new Set();
+dmp.add("Raymond");
+dmp.add("Cynthia");
+dmp.add("Bryan");
+var inter = cis.intersect(dmp);
+console.log(inter.show());
+
+
+console.log("<--- SUBSET --->");
+
+var it = new Set();
+it.add("Cynthia");
+it.add("Clayton");
+it.add("Jennifer");
+it.add("Danny");
+it.add("Jonathan");
+it.add("Terrill");
+it.add("Raymond");
+it.add("Mike");
+var dmp = new Set();
+dmp.add("Cynthia");
+dmp.add("Raymond");
+dmp.add("Jonathan");
+if (dmp.subset(it)) {
+  console.log("DMP is a subset of IT.");
+} else {
+  console.log("DMP is not a subset of IT.");
+}
+
+//No Longer a subset
+dmp.add("Shirley");
+if (dmp.subset(it)) {
+  console.log("DMP is a subset of IT.");
+} else {
+  console.log("DMP is not a subset of IT.");
+}
+
+console.log("<--- DIFFERENCE --->");
+
+var cis = new Set();
+var it = new Set();
+cis.add("Clayton");
+cis.add("Jennifer");
+cis.add("Danny");
+it.add("Bryan");
+it.add("Clayton");
+it.add("Jennifer");
+var diff = new Set();
+diff = cis.difference(it);
+console.log("[" + cis.show() + "] difference [" + it.show() + "] -> [" + diff.show() + "]");
+
+console.log("<--- EXERCICE 1 --->");
+// Modify the Set class so that the class stores its elements in sorted order. Write a program to test your implementation.
+
+Set.prototype.showSorted = function() {
+  return this.data.sort();
+};
+
+var cis = new Set();
+cis.add("Clayton");
+cis.add("Jennifer");
+cis.add("Danny");
+
+console.log(cis.showSorted());
+
+console.log("<--- EXERCICE 2 --->");
+// Addthefunctionhigher(element)totheSetclass.Thisfunctionreturnstheleast element in the set strictly greater than the given element. Test your function in a program.
+
+
+var cis = new Set();
+cis.add(5);
+cis.add(2);
+cis.add(10);
+
+
+Set.prototype.higher = function(element) {
+  this.data.sort(function(a, b) {
+    return a - b;
+  });
+  for (var i = 0; i < this.data.length; i++) {
+    if (this.data[i] > element) {
+      return this.data[i];
+    }
+  }
+  return false;
+};
+
+console.log(cis.higher(2));
+
+console.log("<--- EXERCICE 3 --->");
+// Addthefunctionlower(element)totheSetclass.Thisfunctionreturnsthegreat‐ est element in the set strictly less than the given element. Test your function in a program
+//
+Set.prototype.lower = function(element) {
+  this.data.sort(function(a, b) {
+    return b - a;
+  });
+  for (var i = 0; i < this.data.length; i++) {
+    if (this.data[i] < element) {
+      return this.data[i];
+    }
+  }
+  return false;
+};
+
+console.log(cis.lower(3));
